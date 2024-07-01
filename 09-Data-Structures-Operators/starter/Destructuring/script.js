c2f5ab11a2e40cfd7490a51fea8752de92ad8c48
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-07-01 04:16:45 +0300

    Commit

--- 09-Data-Structures-Operators/starter/Destructuring/script.js
+++ 09-Data-Structures-Operators/starter/Destructuring/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,26 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+
+const [first, , second] = restaurant.categories;
+console.log(first, second);
+
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.mainMenu;
+console.log(starter, mainCourse);
+
+restaurant.order(2, 0);
+const [starter1, mainCourse1] = restaurant.order(2, 0);
+console.log(starter1, mainCourse1);
