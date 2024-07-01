08cb8382fd6df86df967e73c93000526ad265bac
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-07-01 04:15:03 +0300

    Commit

---
+++ 09-Data-Structures-Operators/starter/Destructuring/app001.js
@@ -0,0 +1,38 @@
+'use strict';
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+restaurant.order(2, 0);
+const [starter1, mainCourse1] = restaurant.order(2, 0);
+console.log(starter1, mainCourse1);
+
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
